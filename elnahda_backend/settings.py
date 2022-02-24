6e0f030d1c54886cbd11caa8df304d626755c7cf
Nada494 <nada.taha9411@gmail.com>
2022-02-24 07:26:28 +0200

    edit admin panel

--- elnahda_backend/settings.py
+++ elnahda_backend/settings.py
@@ -131,7 +131,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ElNahda_center',
         'USER': 'postgres',
-        'PASSWORD': 'maryam123',
+        'PASSWORD': '2051999',
         'HOST': 'localhost',
         'PORT': '5432',
     }
