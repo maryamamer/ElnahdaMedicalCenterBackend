4e9450db3dc7c7a23cdcfea4b5a2205ce1762938
Maram Khaled <52356509+MaramRaslan45@users.noreply.github.com>
2022-02-28 17:34:44 +0200

    edit models (#10)

--- user/models.py
+++ user/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import AbstractUser, PermissionsMixin
 import datetime
 
 from django.contrib.auth.base_user import BaseUserManager
-
+from Departments.models import Department
+from Doctors.models import Doctor
 
 class CustomUserManager(BaseUserManager):
 
@@ -38,51 +39,13 @@ class CustomUserManager(BaseUserManager):
 
         return self._create_user(email, password, **extra_fields)
 
-
-class Doctor(models.Model):
-    id = models.AutoField(primary_key=True)
-    fullname = models.CharField(max_length=50)
-    experience=models.IntegerField(default='1')
-    username = models.CharField(max_length=20)
-    email = models.EmailField(max_length=50)
-    image = models.ImageField(upload_to=f'images/{uuid.uuid4()}', blank=True)
-    phone_regex = RegexValidator(
-        regex=r'^01[1|0|2|5][0-9]{8}$', message='phone must be an egyptian phone number...')
-    phone = models.CharField(verbose_name="phone", null=True, validators=[
-                             phone_regex], max_length=14)
-    date_of_birth = models.DateField(null=True)
-    address = models.TextField(null=True)
-    price = models.IntegerField(null=True)
-
-    age = models.IntegerField(null=True)
-    GENDER_CHOICES = (
-        ('male', 'Male'),
-        ('female', 'Female'),
-    )
-    gender = models.CharField(max_length=30, choices=GENDER_CHOICES)
-    education_degree = models.CharField(max_length=30, null=False)
-    specialization = models.CharField(max_length=50, null=False)
-    achievements = models.TextField(null=True)
-    marital_status = (
-        ('single', 'Single'),
-        ('married', 'Married'),
-    )
-    status = models.CharField(max_length=30, choices=marital_status)
    
-
-    # def save(self):
-    #     encodedString = base64.b64encode(self.image.file.read())
-    #     data = {"key": os.environ.get("IMG_BB"), "image": encodedString.decode("utf-8")}
-    #     uploadedImageInfo = requests.post("https://api.imgbb.com/1/upload", data=data)
-    #     jsonResponse = json.loads(uploadedImageInfo.text)
-    #     self.image = jsonResponse["data"]["display_url"]
-    #     super().save()
 
 
 class Customuser(AbstractUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50,unique=True)
-    fullname =models.CharField(max_length=50)
+    fullname =models.CharField(max_length=50,null=True)
     email = models.EmailField("email address", unique=True)
     report = models.FileField(upload_to='uploads/% Y/% m/% d/', null=True)
     phone_regex = RegexValidator(
@@ -105,6 +68,8 @@ class Customuser(AbstractUser, PermissionsMixin):
         ('second degree', 'Second Degree'),
     )
     guardian_relation = models.CharField(max_length=30, choices=Guardian_choices,null=True)
+    class Meta:
+        abstact:True
     USERNAME_FIELD = "username" # make the user log in with the email
     # REQUIRED_FIELDS = ["username"]
 
@@ -117,65 +82,6 @@ class Customuser(AbstractUser, PermissionsMixin):
         return self.email
     
 
-
-    # id=models.AutoField(primary_key=True)
-    # comment = models.CharField(max_length=250,null=True)
-
-    # username=models.CharField(max_length=20)
-    # email=models.EmailField(max_length=50)
-    # password=models.CharField(max_length=16)
-
-
-#     id=models.AutoField(primary_key=True)
-#     fullname=models.CharField(max_length=50)
-#     username=models.CharField(max_length=20)
-#     email=models.EmailField(max_length=50)
-#     report = models.FileField(upload_to ='uploads/% Y/% m/% d/')
-#     phone_regex = RegexValidator(regex=r'^01[1|0|2|5][0-9]{8}$', message='phone must be an egyptian phone number...')
-#     phone = models.CharField(verbose_name="phone",null=True, validators=[phone_regex], max_length=14)
-#     date_of_birth = models.DateField(null=True)
-#     record_number = models.TextField()
-#     address= models.TextField()
-#     age = models.IntegerField()
-#     GENDER_CHOICES = (
-#         ('male', 'Male'),
-#         ('female', 'Female'),
-#     )
-#     gender = models.CharField(max_length=30, choices=GENDER_CHOICES)
-#     guardian = models.CharField(max_length=20,null=True)
-#     Guardian_choices = (
-#         ('first degree', 'First Degree'),
-#         ('second degree', 'Second Degree'),
-#     )
-#     guardian_relation = models.CharField(max_length=30, choices=Guardian_choices)
-
-
-# class Doctor(models.Model):
-#     id=models.AutoField(primary_key=True)
-#     fullname=models.CharField(max_length=50)
-#     username=models.CharField(max_length=20)
-#     email=models.EmailField(max_length=50)
-#     image = models.ImageField(upload_to=f'images/{uuid.uuid4()}', blank=True)
-#     phone_regex = RegexValidator(regex=r'^01[1|0|2|5][0-9]{8}$', message='phone must be an egyptian phone number...')
-#     phone = models.CharField(verbose_name="phone",null=True, validators=[phone_regex], max_length=14)
-#     date_of_birth = models.DateField(null=True)
-#     address= models.TextField()
-#     age = models.IntegerField()
-#     GENDER_CHOICES = (
-#         ('male', 'Male'),
-#         ('female', 'Female'),
-#     )
-#     gender = models.CharField(max_length=30, choices=GENDER_CHOICES)
-#     education_degree = models.CharField(max_length=30,null=False)
-#     specialization = models.CharField(max_length=50,null=False)
-#     achievements = models.TextField(null=True)
-#     marital_status = (
-#         ('single', 'Single'),
-#         ('married', 'Married'),
-#     )
-#     status =  models.CharField(max_length=30, choices=marital_status)
-
-
 class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
@@ -184,19 +90,9 @@ class Appointment(models.Model):
     doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE)
 
 
-class PatientAppointment(models.Model):
-    app_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
-    patient_id = models.ForeignKey(Customuser, on_delete=models.CASCADE)
-
-
-class DoctorAppointment(models.Model):
-    doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    app_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
-
-
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     patient_id = models.ForeignKey(Customuser, on_delete=models.CASCADE)
     doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     comment_description = models.TextField(blank=True, null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True,null=True)
