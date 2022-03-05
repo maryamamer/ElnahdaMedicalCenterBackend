26cf89dd8b8dc6d500d327e96a17ab2375a8f08f
maramkhaled <maramraslan45@gmail.com>
2022-03-05 13:44:44 +0200

    edit userview

--- user/admin.py
+++ user/admin.py
@@ -7,8 +7,8 @@ admin.site.register(Doctor)
 
 class CustomUserAdmin(UserAdmin):
     model = Customuser
-    list_display = ['id','email','phone','date_of_birth','report','address','age','gender','fullname']
-    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('phone','date_of_birth','report','address','age','gender','fullname')}),
+    list_display = ['id','username','email','phone','date_of_birth','address','age','gender','fullname']
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('phone','date_of_birth','address','age','gender','fullname')}),
 )
 
 admin.site.register(Customuser, CustomUserAdmin)
